1fb8327c7690570650e7aed2b5957d86294ded74
vickvey <vivek22@iiserb.ac.in>
2023-12-21 22:00:38 +0530

    added chapter3_variables

---
+++ chp3_var_and_mut/src/main.rs
@@ -0,0 +1,10 @@
+// As you know variables are immutable (constant) by default.
+
+fn main() {
+    let x = 4; // a constant
+
+    println!("Hello, world!");
+
+    // x = 6; // this will give an error as x is immutable(constant)
+    println!("The value of x is: {x}");
+}
---
+++ chp3_var_and_mut/target/debug/chp3_var_and_mut.d
@@ -0,0 +1,1 @@
+/home/vivek/Desktop/Learning-Rust/chp3_var_and_mut/target/debug/chp3_var_and_mut: /home/vivek/Desktop/Learning-Rust/chp3_var_and_mut/src/main.rs
